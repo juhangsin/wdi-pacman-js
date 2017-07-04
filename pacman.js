dacfce48f5e7b62d411efe4208cbaf31b1fcfe89
Ju-Hang Sin <juhangsin@gmail.com>
2017-07-03 21:22:47 -0400

    step 4 completed

--- pacman.js
+++ pacman.js
@@ -60,6 +60,10 @@ function displayStats() {
 function displayMenu() {
 	console.log('\n\nSelect Option:\n');  // each \n creates a new line
 	console.log('(d) Eat Dot');
+	console.log('(1) Eat Inky');
+	console.log('(2) Eat Blinky');
+	console.log('(3) Eat Pinky');
+	console.log('(4) Eat Cyde');
 	console.log('(q) Quit');
 }
 
@@ -75,6 +79,20 @@ function eatDot() {
 	score += 10;
 }
 
+function eatGhost(ghosts) {
+	if (ghost.edible === false) {
+		console.log('\nChomp! ' + ghost.name + ' the ' + ghost.colour + ' ate Pac-Man!');
+		lives -= 1
+	// } else (ghost.edible === true) {
+	// 	console.log('\nChomp! Pac-Man just ate' + ghost.name + ' the ' + ghost.colur);
+	// }
+}}
+
+function gameOver() {
+	if (lives === 0) {
+		process.exit()
+	}
+}
 
 // Process Player's Input
 function processInput(key) {
